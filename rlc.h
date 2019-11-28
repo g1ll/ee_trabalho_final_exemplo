f9132550a4c4c4aa001d9a16d215c71926ebd6b1
g1ll <gillvelleda@gmail.com>
2019-11-28 16:19:20 -0300

    doc rlc.h

--- rlc.h
+++ rlc.h
@@ -1,4 +1,25 @@
-void circuitoRLC(
+
+/**
+ *    Resolve o Circuito Ressonante (RLC) Subamortecido e quarda
+ * a solução no parâmetro de E/S Qa. Qa deve ser
+ * um ponteiro para uma área de merória alocada de 
+ * acordo com a discretização do Tempo(Ti,h e Tf)
+ * 
+ * PARÂMETROS DE ENTRADA
+ * V     Tensão (V)
+ * R     Resistência (Ohm)
+ * L     Indutância (Henry)
+ * C     Capacitância (Farad)
+ * Ti    Tempo inicial (s)
+ * h     Passo de tempo (s)
+ * Tf    Tempo final (s)
+ * 
+ * PARÂMETROS DE ENTRADA/SAÍDA
+ * *T    Ponteiro para vetor de tempos (s)
+ * *Qa   Ponteiro para Carga no Capacitor (Columbs)
+ * 
+ * */
+void circuitoRLC(       
         double V,
         double R,
         double L,
@@ -10,9 +31,31 @@ void circuitoRLC(
         double *Qa);
 
 
-
+/**
+ *   Cria o arquivo .DAT para ser lido pelo script de plotagem.
+ * O arquivo deve se chamar 'rlc.dat' e seguir o padrão do gnuplot
+ * com duas colunas de valores separadas por <TAB> \t
+ * Exemplo:
+ * y1   x1
+ * y2   x2
+ * y3   x3
+ *  ....
+ * 
+ * PARÂMETROS DE ENTRADA
+ * *dataQ    Ponteiro para dados da carga (Qa)
+ * *dataT    Ponteiro para dados dos passos de tempo(s)
+ *  size     Quantidade de dados de acordo com o passo de tempo
+ * 
+ * */
 void createRLCDatFile(double *dataQ,double *dataT,int size);
 
-void creatRLCLogFile(double V, double R, double L, double C,
+
+/**
+ *    
+ *   Criar o arquivo de Log com dos dados (*dataQ, *dataT)
+ *  e os parametros de entrada do circuito RLC (V, R, L, C, Ti, h, Tf)
+ * 
+ * */
+void createRLCLogFile(double V, double R, double L, double C,
                 double Ti,double h, double Tf,
                 double *dataQ,double *dataT,int size);
