619394b07b17f8a0fa765d0025ac2fc27fa29eda
g1ll <gillvelleda@gmail.com>
2019-12-16 11:18:58 -0300

    Open img file

--- main.c
+++ main.c
@@ -5,6 +5,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	FILE *img_file;
+	char *img_name = ".\\plot_rlc.png";
+	
 	system(".\\gnuplotPortable\\gnuplotPortable.exe -c gnuplot_rlc.gnu");
+	
+	img_file = fopen(img_name,"r");
+	if(img_file!=NULL){
+			printf("\nImagem gerada com sucesso!\n\n");
+			printf("\nArquivo: %s\n",img_name);
+			system(img_name);
+	}else{
+		printf("\nErro ao plotar imagem!\n\n");
+	}
+	
 	return 0;
 }
